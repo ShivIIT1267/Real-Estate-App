4cff8220dc3aec57714cb7ef2422c78d12712da5
shivaji rao <rshivaji295@gmail.com>
2025-06-09 17:07:30 +0530

    the back is setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import { react } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage.jsx";
 import Profile from "./pages/profile.jsx";
 import Signin from "./pages/SignIn.jsx";
-
+import Header from "./Components/Header.jsx";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
