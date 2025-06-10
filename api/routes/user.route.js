4f7bee94ece1ef43ac8268dae296b6829830adc0
shivaji rao <rshivaji295@gmail.com>
2025-06-11 02:47:49 +0530

    create update use api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,10 +3,12 @@
 import express from "express";
 
 // we import the required res and req data from another file
-import { test } from "../controllers/user.controllers.js";
+import { test, updateUser } from "../controllers/user.controllers.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
