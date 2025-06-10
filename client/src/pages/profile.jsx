ae28db9657c93f097703446b8cb55927cc0f1137
shivaji rao <rshivaji295@gmail.com>
2025-06-11 00:42:50 +0530

    profile image updation using file choosing

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,12 +1,20 @@
 import { useSelector } from "react-redux";
 
+import { useRef } from "react";
+
+// storing and updating of image to be learnt later on
+// using any free storage
+
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input type="file" ref={fileRef} hidden accept="image/*" />
         <img
+          onClick={() => fileRef.current.click()}
           src={currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
