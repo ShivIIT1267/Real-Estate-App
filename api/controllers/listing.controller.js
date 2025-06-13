82d5955426a022ac0e98bd8f6e0f7523d0b8b756
shivaji rao <rshivaji295@gmail.com>
2025-06-14 03:53:47 +0530

    api for updating the listings

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -23,6 +24,30 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     return res.status(200).json({ message: "Listing deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return res.status(404).json({ message: "Listing not found" });
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(403, "You can update only your own listings"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      // important for doing updates in place
+      { $set: req.body },
+      { new: true }
+    );
+
+    return res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
